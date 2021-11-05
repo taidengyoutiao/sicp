b240ccfc29e1b01f045b25b69aefc13fab0797f3
liyingfu <yinfu1358@163.com>
2021-11-05 13:40:13 +0800

    H3

--- excerise/c1/1.6-1.8.rkt
+++ excerise/c1/1.6-1.8.rkt
@@ -1,19 +1,20 @@
 #lang racket
 ; newton's methods for square root
 
-
-
-; step1: Knowing all the things that happed, happening and will happen. Find the patterns/rules.
-; step2: write the "description" in natural language about the rules in step1.
-; step3: translate the "description" into programming lauguage which is the "program".
-; sometimes, step2 is omitted.
+; step1: Knowing all the things that happed, happening and will happen. Find the patterns/rules
+; step2: write the "description" in natural language about the rules in step1
+; step3: translate the "description" into programming lauguage which is the "program"
+; sometimes, step2 is omitted
 
 ; knack of step1:
-; try to know what happend in each iteration
-; try to use variables to stands for something
+; try to find the iteration/recursion part of the problem
+; try to know what happend in each iteration(if I know all the things in one iteration, the function is about to be written down.)
+; variables stand for stable meanings
+; use auxiliary procedure to do things
 
-; write code from top to bottom.
+; write code from top to bottom
 ; start from the arguments and function name/meaning
+
 (define (sq guess x)
   (if (good-enough? guess x)
       x
