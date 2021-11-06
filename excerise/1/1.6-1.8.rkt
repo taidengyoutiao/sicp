7877073a134b85cb0051b1c409182671ec9fe2c6
liyingfu <yinfu1358@163.com>
2021-11-06 10:31:51 +0800

    H5

--- excerise/1/1.6-1.8.rkt
+++ excerise/1/1.6-1.8.rkt
@@ -15,21 +15,38 @@
 ; write code from top to bottom
 ; start from the arguments and function name/meaning
 
-;(define (sq guess x)
-  ;(if (good-enough? guess x)
-      ;x
-      ;(sq (improve guess x) x)))
-
-; to-do: 1.6
-
-; 1.7
+; Newton's methods for square root
+(define (sqrt x) (sqrt-iter 1.0 x))
+(define (sqrt-iter guess x)
+  (if (good-enough? guess x)
+      guess
+      (sqrt-iter (improve guess x) x)))
+(define (improve guess x)
+  (average guess (/ x guess)))
+(define (average x y) (/ (+ x y) 2))
 (define (abs x) (if (< x 0)
                     (- x) x))
 (define (square x)
   (* x x))
 (define (good-enough? guess x)
   (< (abs (- (square guess) x)) 0.001))
+
+; todo: 1.6
+
+; half: 1.7
 (define (good-enough?* guess old-guess)
   (< (/ (abs (- guess old-guess)) guess) 0.001))
 
-(good-enough?* 100 100)
+; done: 1.8
+(define (improve-cube guess x)
+  (/ (+ (/ x (* guess guess)) (* 2 guess)) 3))
+(define (good-enough-cube? guess x)
+  (< (abs (- (cube guess) x)) 0.001))
+(define (cube x)
+  (* x x x))
+(define (curt-iter guess x)
+  (if (good-enough-cube? guess x)
+      guess
+      (curt-iter (improve-cube guess x) x)))
+(define (curt x)
+  (curt-iter 1 x))
